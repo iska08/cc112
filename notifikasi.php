78469f9b405c89a14ac2cfd674c11e9c39efdc94
RifqiAN <arimakouseireborn12@gmail.com>
2023-07-24 09:55:13 +0700

    day 16

--- notifikasi.php
+++ notifikasi.php
@@ -1,23 +1,21 @@
 <script type="text/javascript">
-    function tampil_button_notif() {
-           $.ajax({
-                url:"<?php echo base_url() ?>index.php/peminjaman_buku/untuk_buttonnya",
-                method: 'post',
-                dataType: 'json',
-                success: function(data)
-                {
-                    $("#button_notif").val(data.jumlah_total);
-                    console.log(data.jumlah_total)
-                }
-            });
-          }  
-          function tampil_isi_notif() {
-            $.ajax({
-            url:"<?php echo base_url() ?>index.php/peminjaman_buku/untuk_isinya",
-            success: function(html)
-            { 
-            $('#button_isi').html(html);
-            }
-            });
-          }    
+  function tampil_button_notif() {
+    $.ajax({
+      url:"<?php echo base_url() ?>index.php/peminjaman_buku/untuk_buttonnya",
+      method: 'post',
+      dataType: 'json',
+      success: function(data) {
+        $("#button_notif").val(data.jumlah_total);
+        console.log(data.jumlah_total)
+      }
+    });
+  }  
+  function tampil_isi_notif() {
+    $.ajax({
+      url:"<?php echo base_url() ?>index.php/peminjaman_buku/untuk_isinya",
+      success: function(html) {
+        $('#button_isi').html(html);
+      }
+    });
+  }
 </script>
