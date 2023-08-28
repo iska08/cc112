08b2aca6bed285777c4abcdd5dd9d3651dfc7667
rizqi2041721099 <rizqiahmad939@gmail.com>
2023-08-28 07:49:21 +0700

    .

--- cypress/e2e/AddUser.cy.js
+++ cypress/e2e/AddUser.cy.js
@@ -10,11 +10,14 @@ describe('Login', () => {
       cy.get('.mb-3 > .form-control').type('cc112admin')
       cy.get(':nth-child(2) > .form-control').type('password')
       cy.get('.btn').click()
-
+      cy.get('.main-header > :nth-child(1) > :nth-child(3) > .nav-link').click()
+      // cy.get('.nav-pills > :nth-child(2) > [href="#"]').click()
+      // cy.get('.menu-is-opening > .nav > :nth-child(1) > .nav-link').click()
+      // cy.get('#add_lokasi').click()
+      // cy.get('#latlong').type('LatLng(-7.010412, 113.87464)')
+      // cy.get(':nth-child(2) > .form-control').select('BENCANA ALAM')
       // cy.get('.main-header > :nth-child(1) > :nth-child(1) > .nav-link').click()
       // cy.get('.nav-pills > :nth-child(2) > [href="#"]').click()
-
-      // Logout
       // cy.get('.main-header > :nth-child(1) > :nth-child(3) > .nav-link').click()
     })
   })
---
+++ cypress/e2e/test_datalokasi_lokasikejadian.cy.js
@@ -0,0 +1,18 @@
+describe('test_datalokasi_inputlokasi', () => {
+    Cypress.on('uncaught:exception', (err, runnable) => {
+      console.log(err);
+      return false;
+    })
+    it('data lokasi kejadian', () => {
+        cy.visit('http://localhost/cc112/')
+        cy.get('.nav-link').click()
+        cy.get('.mb-3 > .form-control').type('cc112admin')
+        cy.get(':nth-child(2) > .form-control').type('password')
+        cy.get('.btn').click()
+        cy.get('.nav-pills > :nth-child(2) > [href="#"]').click()
+        cy.get('.menu-is-opening > .nav > :nth-child(1) > .nav-link').click()
+        cy.get('.custom-select').select('20')
+        // cy.get('#tower_next > .page-link').click()
+
+    })
+})
