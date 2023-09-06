e2a434c67299b540e200180617a73cf4dfc074ea
iska08 <aamuhaimin08@gmail.com>
2023-09-06 19:12:56 +0700

    .

--- admcc112.php
+++ admcc112.php
@@ -182,8 +182,8 @@ if (empty($_SESSION['112_username'])){
                     <nav class="mt-2">
                         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                             <!-- Add icons to the links using the .nav-icon class with font-awesome or any other icon font library -->
-                            <li class="nav-item ">
-                                <a href="../" class="nav-link">
+                            <!-- <li class="nav-item ">
+                                <a href="../admcc112.php" class="nav-link">
                                     <i class="nav-icon fas fa-tachometer-alt"></i>
                                     <p>
                                         Dashboard
@@ -198,6 +198,12 @@ if (empty($_SESSION['112_username'])){
                                         </a>
                                     </li>
                                 </ul>
+                            </li> -->
+                            <li class="nav-item">
+                                <a href="admcc112.php" class="nav-link">
+                                    <i class="nav-icon fas fa-tachometer-alt"></i>
+                                    <p class="text">Dashboard</p>
+                                </a>
                             </li>
                             <li class="nav-item">
                                 <a href="#" class="nav-link">
