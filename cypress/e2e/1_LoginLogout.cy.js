2455a4f47ea3d4aa030800cee52b68115f28a7cf
rizqi2041721099 <rizqiahmad939@gmail.com>
2023-09-26 08:37:40 +0700

    .

--- cypress/e2e/1_LoginLogout.cy.js
+++ cypress/e2e/1_LoginLogout.cy.js
@@ -4,24 +4,21 @@ describe('Login', () => {
     return false;
   })
   it('Login Admin', () => {
-    cy.visit('http://localhost/cc112/')
-    cy.get('.nav-link').click()
+    cy.visit('http://localhost/cc112/login.php')
     cy.get('.mb-3 > .form-control').type('cc112admin')
     cy.get(':nth-child(2) > .form-control').type('password')
     cy.get('.btn').click()
     cy.get('.main-header > :nth-child(1) > :nth-child(3) > .nav-link').click()
   })
   it('Login Tim', () => {
-    cy.visit('http://localhost/cc112/')
-    cy.get('.nav-link').click()
+    cy.visit('http://localhost/cc112/login.php')
     cy.get('.mb-3 > .form-control').type('damkar_CC112')
     cy.get(':nth-child(2) > .form-control').type('password')
     cy.get('.btn').click()
     cy.get('.main-header > :nth-child(1) > :nth-child(3) > .nav-link').click()
   })
   it('Login Tim', () => {
-    cy.visit('http://localhost/cc112/')
-    cy.get('.nav-link').click()
+    cy.visit('http://localhost/cc112/login.php')
     cy.get('.mb-3 > .form-control').type('1CallCenter112')
     cy.get(':nth-child(2) > .form-control').type('password')
     cy.get('.btn').click()
